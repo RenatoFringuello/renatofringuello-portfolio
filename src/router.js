618b6f270e1d052176e2d119dcbeeb360e8d3ccf
RenatoFringuello <renatofringuello@hotmail.it>
2023-09-04 18:38:33 +0200

    add bootstrap js and fontawesome css

---
+++ src/router.js
@@ -0,0 +1,27 @@
+import { createRouter, createWebHistory } from "vue-router";
+
+import AppPageStatusCode from './pages/AppPageStatusCode.vue';
+import AppHome from './pages/AppHome.vue';
+// import AppProjects from './pages/AppProjects.vue';
+// import AppAbout from './pages/AppAbout.vue';
+
+const router = createRouter({
+    history : createWebHistory(),
+    routes : [
+        {
+            path : '/',
+            name : 'home',
+            component : AppHome
+        },
+        {
+            path: '/404', 
+            name: '404', 
+            component: AppPageStatusCode
+        },
+        {
+            path: '/:catchAll(.*)', redirect:'404'
+        },
+    ],
+});
+
+export {router};
